646cd4a74cd191a5f60496e1cd429e6ea141eda8
Paul Dickens <dickens.paul@gmail.com>
2017-06-10 21:58:06 +1000

    added FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EntryDetailsComponent } from './entries/entry-details/entry-details.component';
@@ -12,7 +13,8 @@ import { EntryListComponent } from './entries/entry-list/entry-list.component';
     EntryListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
