1766c9f6213c8d579ef3f45cd6d38e5b144c77b0
Paul Dickens <dickens.paul@gmail.com>
2017-06-11 09:35:27 +1000

    troubleshooting

--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -41,7 +41,7 @@ export class EntryListComponent implements OnInit {
   }
 
   selectEntry(entry: Entry) {
-    console.log('selected ' + entry.toString());
+    console.log('selected ' + entry.timestamp);
     this.selectedEntry = entry
   }
 
