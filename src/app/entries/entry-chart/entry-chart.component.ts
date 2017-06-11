e714d4ee8b0d8ccb469ef9c7234f6ce62fa22047
Paul Dickens <dickens.paul@gmail.com>
2017-06-11 18:18:38 +1000

    changed chart options

--- src/app/entries/entry-chart/entry-chart.component.ts
+++ src/app/entries/entry-chart/entry-chart.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class EntryChartComponent {
-  public scatter_ChartData = [
+  public chartData = [
       ['Date', 'Sales Percentage'],
       [new Date(2016, 3, 22), 78],
       [new Date(2016, 3, 21, 9, 30, 2), 88],
@@ -18,13 +18,33 @@ export class EntryChartComponent {
       [new Date(2016, 3, 16, 15, 40, 35), 68]
   ];
 
-  public scatter_ChartOptions = {
+  public chartOptions = {
       legend: {
           position: 'bottom'
       },
-      title: 'Company Sales Percentage'
+      title: "Solastat data",
+      // hAxis: {title: 'time (s)', minValue: minVal, maxValue: maxVal},
+      hAxis: {title: 'time (s)'},
+      vAxes: [{
+        title: 'temperature (\u2103)',
+        minValue: 0,
+        maxValue: 100
+      }, {
+        minValue: 0,
+        maxValue: 1
+      }],
+      interpolateNulls: false,
+      lineWidth: 1,
+      pointSize: 0,
+      seriesType: 'line',
+      series: [
+        {color: 'red', targetAxisIndex: 0},
+        {color: 'green', targetAxisIndex: 0},
+        {color: 'blue', targetAxisIndex: 0},
+        {type: 'area', lineWidth: 0, targetAxisIndex: 1},
+        {type: 'area', lineWidth: 0, targetAxisIndex: 1}
+      ]
   };
-
 
   constructor() { }
 
